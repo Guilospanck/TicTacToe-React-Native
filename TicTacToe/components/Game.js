128049d11e68cdb0da7c33043e9d92cb53fb5f3e
Guilherme R Pereira <guilherme.rp2014@gmail.com>
2019-08-12 17:26:02 -0300

    Icons and table added in Game.js

--- TicTacToe/components/Game.js
+++ TicTacToe/components/Game.js
@@ -7,29 +7,101 @@ import {
     Switch
 } from 'react-native'
 
+import Icon from 'react-native-vector-icons/Ionicons'
+
 import GLOBALS from './Globals'
 
 export default class Game extends Component {
 
     constructor() {
         super();
+        this.state = {
+            isDarkMode: false
+        }
         GLOBALS.storeData('isInGame', true);
     }
 
     componentDidMount() {
+        GLOBALS.getStoreData('darkMode').then((value) => {
+            this.setState({
+                isDarkMode: value
+            });
+        });
+
         GLOBALS.storeData('isInGame', true);
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         GLOBALS.storeData('isInGame', false);
     }
 
     render() {
         return (
-            <View>
-                <Text>Game works! {this.props.gameMode}</Text>
+            <View style={this.state.isDarkMode ? stylesDarkMode.container : stylesLightMode.container}>
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderLeftWidth: 0, borderTopWidth: 0}]}>
+                    <Icon name="md-close" style={stylesLightMode.tileX}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderTopWidth: 0}]}>
+                    <Icon name="md-radio-button-off" style={stylesLightMode.tileO}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderRightWidth: 0, borderTopWidth: 0}]}></TouchableOpacity>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderLeftWidth: 0}]}></TouchableOpacity>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {}]}></TouchableOpacity>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderRightWidth: 0}]}></TouchableOpacity>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderLeftWidth: 0, borderBottomWidth: 0}]}></TouchableOpacity>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderBottomWidth: 0}]}></TouchableOpacity>
+                    <TouchableOpacity style={[(this.state.isDarkMode ? stylesDarkMode.tile : stylesLightMode.tile), {borderBottomWidth: 0, borderRightWidth: 0}]}></TouchableOpacity>
+                </View>
             </View>
         );
     };
 
 }
+
+const stylesLightMode = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    tile: {
+        borderWidth: 2,
+        width: 100,
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    tileX: {
+        color: 'red',
+        fontSize: 60
+    },
+    tileO: {
+        color: 'green',
+        fontSize: 60
+    }
+});
+
+const stylesDarkMode = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: GLOBALS.DARK_MODE.primaryLight
+    },
+    tile: {
+        borderWidth: 2,
+        width: 100,
+        height: 100,
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: '#6200EE'
+    }
+});
